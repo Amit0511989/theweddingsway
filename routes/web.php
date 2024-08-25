5d67a091cffba93eb66f7e0386e36b21c3f74a4e
amit0511989 <amit0511989@gmail.com>
2024-08-25 12:09:22 +0530

    user

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use App\Http\Controllers\Admin\LocationController;
 use App\Http\Controllers\Admin\SettingController;
 use App\Http\Controllers\Admin\BlogController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\Auth\VerificationController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,61 +23,76 @@ use App\Http\Controllers\Auth\RegisterController;
 |
 */
 
-
-
+// User Authentication Routes
 Auth::routes();
 
+// Manually Adding Auth Routes (Optional)
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('register', [RegisterController::class, 'create']);
+
+Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+
+Route::get('email/verify', [VerificationController::class, 'show'])->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', [VerificationController::class, 'verify'])->name('verification.verify');
+Route::post('email/resend', [VerificationController::class, 'resend'])->name('verification.resend');
+
+// Home Route
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-
-Route::prefix('admin')  ->group(function() {
+// Admin Routes
+Route::prefix('admin')->group(function() {
     Route::get('/', [App\Http\Controllers\Admin\Auth\LoginController::class, 'showLoginForm'])->name('admin.login_form');
     Route::post('/login', [App\Http\Controllers\Admin\Auth\LoginController::class, 'login'])->name('admin.login');
     Route::post('/logout', [App\Http\Controllers\Admin\Auth\LoginController::class, 'logout'])->name('logout');
 
     Route::middleware('auth:admin')->group(function() {
         Route::get('/dashboard', [App\Http\Controllers\Admin\AdminController::class, 'index'])->name('admin.dashboard');
-   
+
+        // User Routes
         Route::get('/users', [UserController::class, 'index'])->name('users_listing');
-        Route::get('/user_listing', [UserController::class, 'index'])->name('user_listing');  
+        Route::get('/user_listing', [UserController::class, 'index'])->name('user_listing');
         Route::get('/add_user', [UserController::class, 'create'])->name('add_user');
         Route::post('/save_user', [UserController::class, 'store'])->name('save_user');
         Route::get('/user_delete/{id}', [UserController::class, 'destroy'])->name('user_delete');
         Route::get('/user_edit/{id}', [UserController::class, 'edit'])->name('user_edit');
         Route::post('/user_update/{id}', [UserController::class, 'update'])->name('user_update');
 
-        Route::get('/roles', [UserController::class, 'roles'])->name('roles');  
+        // Role Routes
+        Route::get('/roles', [UserController::class, 'roles'])->name('roles');
         Route::get('/add_role', [UserController::class, 'addRole'])->name('add_role');
         Route::post('/save_role', [UserController::class, 'saveRole'])->name('save_role');
 
-
-        Route::get('/locations', [LocationController::class, 'index'])->name('location');  
+        // Location Routes
+        Route::get('/locations', [LocationController::class, 'index'])->name('location');
         Route::get('/create', [LocationController::class, 'create'])->name('add_location');
         Route::post('/save_location', [LocationController::class, 'store'])->name('save.location');
 
-        Route::get('/blogs', [BlogController::class, 'index'])->name('blog_listing'); 
+        // Blog Routes
+        Route::get('/blogs', [BlogController::class, 'index'])->name('blog_listing');
         Route::get('/add_blog', [BlogController::class, 'create'])->name('add_blog');
         Route::post('/save_blog', [BlogController::class, 'store'])->name('save_blog');
         Route::get('/blog_delete/{id}', [BlogController::class, 'destroy'])->name('blog_delete');
         Route::get('/blog_edit/{id}', [BlogController::class, 'edit'])->name('blog_edit');
         Route::post('/blog_update/{id}', [BlogController::class, 'update'])->name('blog_update');
-       
-        
 
+        // Settings Routes
         Route::get('/logo_settings', [SettingController::class, 'logo_setting'])->name('logo_setting');
         Route::post('/save_logo_details', [SettingController::class, 'save_logo_settings'])->name('save_logo_details');
-       
     });
-   
 });
 
-       
-
-
+// Command Routes
 Route::get('/run-command', function () {
     // Replace 'your:command' with the Artisan command you want to run
     Artisan::call('storage:link');
-    
+
     // Optionally, return a response or the output of the command
     return Artisan::output();
 });
@@ -80,7 +100,7 @@ Route::get('/run-command', function () {
 Route::get('/run-migrations', function () {
     // Run the migrations
     Artisan::call('migrate');
-    
+
     // Return the output of the migration process
     return Artisan::output();
 });
